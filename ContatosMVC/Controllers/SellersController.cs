3804e81fbc5672715ea2995af80380e3ffc4d5c3
Leandro Santos <leandro22.js@gmail.com>
2024-05-25 14:13:47 -0300

    Criado SellersController e View

---
+++ ContatosMVC/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContatosMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
